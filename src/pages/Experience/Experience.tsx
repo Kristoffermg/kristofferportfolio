b5e0a98870f6983a35cac0f4180128845c165e75
Kristoffer <33570390+Kristoffermg@users.noreply.github.com>
2025-03-31 18:13:35 +0200

    Legend box
    Land cover labels + minor text change

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -19,12 +19,13 @@ export default function Experience() {
       <h3>ClearSky Vision (Oct 2023 - Present)</h3>
       <p>
         As a Junior Developer at ClearSky Vision, I collaborated with another
-        developer on various tasks. My primary contribution is designing and
-        implementing a scene classification model from the ground up, ensuring
-        it works with any image size while maximizing accuracy across different
-        geographies and seasons. Below, you can view a demo of the model in
-        action.
+        developer on various tasks. My primary contribution involves designing
+        and implementing a scene classification model from the ground up, making
+        sure it works with any image size while maximizing accuracy across
+        different geographies and seasons. Below, you can view a demo of the
+        model in action.
       </p>
+
       <div className="demo-container">
         <button
           onClick={() => setIsDropdownOpen(!isDropdownOpen)}
@@ -36,21 +37,48 @@ export default function Experience() {
       {isDropdownOpen && (
         <motion.div
           className="clearskyImageShowcase"
-          initial={{ opacity: 0, y: 20 }} // Initial state (hidden and slightly below)
-          animate={{ opacity: 1, y: 0 }} // Final state (fully visible and at its normal position)
-          transition={{ duration: 0.5 }} // Duration of the animation
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
         >
-          <ReactCompareSlider
-            itemOne={
-              <ReactCompareSliderImage src={clearskyReal} alt="Real image" />
-            }
-            itemTwo={
-              <ReactCompareSliderImage
-                src={clearskySceneClassification}
-                alt="Scene classification"
-              />
-            }
-          />
+          <div className="image-container">
+            <ReactCompareSlider
+              itemOne={
+                <ReactCompareSliderImage src={clearskyReal} alt="Real image" />
+              }
+              itemTwo={
+                <ReactCompareSliderImage
+                  src={clearskySceneClassification}
+                  alt="Scene classification"
+                />
+              }
+            />
+            <div className="color-legend-overlay">
+              <h4>Land Cover Labels</h4>
+              <ul>
+                <li>
+                  <span className="legend-box buildingsAndRoads"></span>{" "}
+                  Buildings & Roads
+                </li>
+                <li>
+                  <span className="legend-box trees"></span> Trees
+                </li>
+                <li>
+                  <span className="legend-box grassland"></span> Grassland
+                </li>
+                <li>
+                  <span className="legend-box agriculture"></span> Agriculture
+                </li>
+                <li>
+                  <span className="legend-box water"></span> Water
+                </li>
+                <li>
+                  <span className="legend-box non-vegetated"></span>{" "}
+                  Non-vegetated
+                </li>
+              </ul>
+            </div>
+          </div>
         </motion.div>
       )}
       <hr />
